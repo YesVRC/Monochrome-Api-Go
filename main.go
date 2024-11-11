f0352a33fe7ef0f49f37b2bbd5aba026a259fd46
YesVRC <altair9090@gmail.com>
2024-11-11 17:08:38 -0500

    Added Test Endpoint

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprintln(w, "Hello World")
 	})
+
+	s.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintln(w, "Test 1234")
+	})
 	err := http.ListenAndServe(":8080", s)
 	if err != nil {
 		panic(err)
